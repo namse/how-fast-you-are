1c4146ab0a8faeae221c62818f8980e0f07ab323
bigfood <satcom1600@hanmail.net>
2019-01-21 17:48:12 +0900

    Make prototype of rhythm game theme

--- client/src/index.ts
+++ client/src/index.ts
@@ -19,10 +19,11 @@ let average = 0;
 let total = 0;
 let max = 0;
 
-const arrowElement = document.getElementById("arrow");
-const totalElement = document.getElementById('total');
+// const arrowElement = document.getElementById("arrow");
+// const totalElement = document.getElementById('total');
 const speedElement = document.getElementById('speed');
 const maxElement = document.getElementById('max');
+const gaugeElement = document.getElementById('gauge_mid');
 
 socket.on('key', async (key) => {
   average += 1;
@@ -43,16 +44,18 @@ function padLeft0(num: number) {
 }
 
 function updateInfo() {
-  totalElement.innerText = `${total}`;
+  // totalElement.innerText = `${total}`;
   speedElement.innerText = `${padLeft0(average * 60)}`;
-  maxElement.innerText = `${max * 60}`;
+  maxElement.innerText = `${padLeft0(max * 60)}`;
 }
 
 function tick() {
-  const degree = 240 / 30 * average - 120;
+  const pixelY = 440 - (380 / 30 * average);
+  // const degree = 240 / 30 * average - 120;
   // console.log(degree);
   updateInfo();
-  arrowElement.style.transform = `rotate(${degree}deg)`;
+  // arrowElement.style.transform = `rotate(${degree}deg)`;
+  gaugeElement.style.clip = `rect(${pixelY}px, 300px, 440px, 0px)`;
   requestAnimationFrame(tick);
 }
 tick();
