cbeba3c1cdf07588e71656d10d8beb3d17f4ba40
bigfood <satcom1600@hanmail.net>
2019-01-21 19:08:24 +0900

    Display total key pressed as combo

--- client/src/index.ts
+++ client/src/index.ts
@@ -18,16 +18,20 @@ socket.on('refresh', () => {
 let average = 0;
 let total = 0;
 let max = 0;
+let previousTime = 0;
+let comboBoxSize = 0;
 
 // const arrowElement = document.getElementById("arrow");
-// const totalElement = document.getElementById('total');
+const totalElement = document.getElementById('total');
 const speedElement = document.getElementById('speed');
 const maxElement = document.getElementById('max');
 const gaugeElement = document.getElementById('gauge_mid');
+const comboBoxElement = document.getElementById('comboBox');
 
 socket.on('key', async (key) => {
   average += 1;
   total += 1;
+  popupComboBox();
   odometer.update(total);
   console.log(key);
   setTimeout(() => {
@@ -44,20 +48,37 @@ function padLeft0(num: number) {
 }
 
 function updateInfo() {
-  // totalElement.innerText = `${total}`;
+  totalElement.innerText = `${total}`;
   speedElement.innerText = `${padLeft0(average * 60)}`;
   maxElement.innerText = `${padLeft0(max * 60)}`;
+  comboBoxElement.style.fontSize = `${comboBoxSize}px`;
+}
+
+function popupComboBox() {
+  comboBoxSize = 50;
+}
+
+function shrinkComboBox(dt: number) {
+  if(!comboBoxSize) return;
+  comboBoxSize -= 150 * dt;
+  if(comboBoxSize >= 0) return;
+  comboBoxSize = 0; 
 }
 
 function tick() {
+  const currentTime = Date.now();
+  const dt = (currentTime - previousTime) / 1000;
+  previousTime = currentTime;
   const pixelY = 440 - (380 / 30 * average);
   // const degree = 240 / 30 * average - 120;
   // console.log(degree);
+  shrinkComboBox(dt)
   updateInfo();
   // arrowElement.style.transform = `rotate(${degree}deg)`;
   gaugeElement.style.clip = `rect(${pixelY}px, 300px, 440px, 0px)`;
   requestAnimationFrame(tick);
 }
+previousTime = Date.now();
 tick();
 
 
